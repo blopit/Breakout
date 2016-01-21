e948726ac67ea6d0b83383c636bf2ee15f7fff21
Shrenil Patel <patel.shrenil@gmail.com>
2016-01-20 21:13:38 -0500

    bounce math

--- src/Game.java
+++ src/Game.java
@@ -6,72 +6,105 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
+public class Game extends JPanel implements Runnable {
+	private static final long serialVersionUID = -9L;
+	boolean running = true;
+	Thread animator;
 
-public class Game extends JPanel{
-	
 	int totalFrameCount = 0;
-	ActionListener updateFPS = new ActionListener()
-    {   
-        @Override
-        public void actionPerformed(ActionEvent event)
-        {
-	        System.out.println(totalFrameCount);
-	        totalFrameCount = 0;
-	    }
+	int current_fps = 0;
+	ActionListener updateFPS = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			current_fps = totalFrameCount;
+			totalFrameCount = 0;
+		}
 	};
-	
-	
+
 	double lastUpdateTime = System.nanoTime();
 	private int pos_x = 0;
 	private int pos_y = 0;
-	private int fps = 60;
-	private Timer timer;
-	
+
 	private Ball ball;
 	private Paddle paddle;
 	private int screen_width = 800;
 	private int screen_height = 600;
-	private static ArrayList<Rect> rectList;
-	
-	boolean collisionRectRect(double AX1 , double AY1, double AX2, double AY2, double BX1 , double BY1, double BX2, double BY2)
-	{
-		return (AX1 <= BX2 && AX2 >= BX1 &&
-			    AY1 <= BY2 && AY2 >= BY1); 
+	private static ArrayList<Block> blockList;
+
+	public void addNotify() {
+		super.addNotify();
+		animator = new Thread(this);
+		animator.start();
+	}
+
+	private float dist(double d, double e, double camx2, double camy2) {
+		return (float) Math.sqrt(Math.pow(camx2-d,2) + Math.pow(camy2-e,2));
 	}
 	
-	boolean collisionCircleLine(Ball b, double lineAX, double lineAY, double lineBX, double lineBY) {
-		
-		if (!collisionRectRect(b.x-b.radius,b.y-b.radius,b.x+b.radius,b.y+b.radius,
-				lineAX,lineAY,lineBX,lineBY))
-			return false;
-		
-		double circleCenterX = b.x; 
-		double circleCenterY = b.y; 
-		double circleRadius = b.radius;
-		
-	    double lineSize = Math.sqrt(Math.pow(lineAX-lineBX, 2) + Math.pow(lineAY-lineBY, 2));
-	    double distance;
-
-	    if (lineSize == 0) {
-	        distance = Math.sqrt(Math.pow(circleCenterX-lineAX, 2) + Math.pow(circleCenterY-lineAY, 2));
-	        return distance < circleRadius;
-	    }
-
-	    double u = ((circleCenterX - lineAX) * (lineBX - lineAX) + (circleCenterY - lineAY) * (lineBY - lineAY)) / (lineSize * lineSize);
-
-	    if (u < 0) {
-	        distance = Math.sqrt(Math.pow(circleCenterX-lineAX, 2) + Math.pow(circleCenterY-lineAY, 2));
-	    } else if (u > 1) {
-	        distance = Math.sqrt(Math.pow(circleCenterX-lineBX, 2) + Math.pow(circleCenterY-lineBY, 2));
-	    } else {
-	        double ix = lineAX + u * (lineBX - lineAX);
-	        double iy = lineAY + u * (lineBY - lineAY);
-	        distance = Math.sqrt(Math.pow(circleCenterX-ix, 2) + Math.pow(circleCenterY-iy, 2));
-	    }
-
-	    return distance < circleRadius;
+	private boolean collisionRectRect(double AX1, double AY1, double AX2, double AY2,
+			double BX1, double BY1, double BX2, double BY2) {
+		return (AX1 <= BX2 && AX2 >= BX1 && AY1 <= BY2 && AY2 >= BY1);
 	}
-	
+
+	private boolean collisionCircleLine(double circleCenterX, double circleCenterY,
+			double circleRadius, double lineAX, double lineAY, double lineBX,
+			double lineBY) {
+
+		/*
+		 * if
+		 * (!collisionRectRect(b.x-b.radius,b.y-b.radius,b.x+b.radius,b.y+b.radius
+		 * , lineAX,lineAY,lineBX,lineBY)) return false;
+		 */
+
+		// double circleCenterX = b.x;
+		// double circleCenterY = b.y;
+		// double circleRadius = b.radius;
+
+		double lineSize = Math.sqrt(Math.pow(lineAX - lineBX, 2)
+				+ Math.pow(lineAY - lineBY, 2));
+		double distance;
+
+		if (lineSize == 0) {
+			distance = Math.sqrt(Math.pow(circleCenterX - lineAX, 2)
+					+ Math.pow(circleCenterY - lineAY, 2));
+			return distance < circleRadius;
+		}
+
+		double u = ((circleCenterX - lineAX) * (lineBX - lineAX) + (circleCenterY - lineAY)
+				* (lineBY - lineAY))
+				/ (lineSize * lineSize);
+
+		if (u < 0) {
+			distance = Math.sqrt(Math.pow(circleCenterX - lineAX, 2)
+					+ Math.pow(circleCenterY - lineAY, 2));
+		} else if (u > 1) {
+			distance = Math.sqrt(Math.pow(circleCenterX - lineBX, 2)
+					+ Math.pow(circleCenterY - lineBY, 2));
+		} else {
+			double ix = lineAX + u * (lineBX - lineAX);
+			double iy = lineAY + u * (lineBY - lineAY);
+			distance = Math.sqrt(Math.pow(circleCenterX - ix, 2)
+					+ Math.pow(circleCenterY - iy, 2));
+		}
+
+		return distance < circleRadius;
+	}
+
+	private class Line {
+		private int AX;
+		private int AY;
+		private int BX;
+		private int BY;
+		private Block rect;
+		public Line(Block r, int ax, int ay, int bx, int by) {
+			AX = ax;
+			AY = ay;
+			BX = bx;
+			BY = by;
+			rect = r;
+		}
+	}
+
 	private class Rect {
 		protected int x;
 		protected int y;
@@ -79,176 +112,301 @@ public class Game extends JPanel{
 		protected int height;
 		protected double rotation;
 		
-		public Rect(int x, int y, int width, int height){
+		public Rect(int x, int y, int width, int height) {
 			this.x = x;
 			this.y = y;
 			this.width = width;
 			this.height = height;
 			this.rotation = 0;
+			
 		}
 	}
-	
-	private class Block extends Rect{
+
+	private class Block extends Rect {
 		private int hp;
+		protected Line[] lines;
+
 		public Block(int x, int y, int width, int height, int hp) {
 			super(x, y, width, height);
 			this.hp = hp;
+			this.lines = new Line[] {
+					new Line(this, this.x, this.y, this.x + this.width, this.y),
+					new Line(this,this.x + this.width, this.y, this.x + this.width,
+							this.y + this.height),
+					new Line(this,this.x + this.width, this.y + this.height, this.x,
+							this.y + this.height),
+					new Line(this,this.x, this.y + this.height, this.x, this.y)};
 		}
 	}
-	
-	private class Paddle extends Rect{
+
+	private class Paddle extends Rect {
 		private int hsp;
+
 		public Paddle(int x, int y, int width, int height) {
 			super(x, y, width, height);
 			this.hsp = 32;
 		}
-		private void moveTo(int dx){
-			
-			if (Math.abs(dx-this.x) > this.hsp){
-				this.x += this.hsp*Math.signum((double)(dx-this.x));
-			}else{
+
+		private void moveTo(int dx) {
+
+			if (Math.abs(dx - this.x) > this.hsp) {
+				this.x += this.hsp * Math.signum((double) (dx - this.x));
+			} else {
 				this.x = dx;
 			}
 		}
 	}
-	
-	private class Ball{
+
+	private class Ball {
 		private int x;
 		private int y;
-		private int xsp;
-		private int ysp;
+		private double xsp;
+		private double ysp;
 		private int radius;
-		public Ball(int xx, int yy){
+
+		public Ball(int xx, int yy) {
 			this.x = xx;
 			this.y = yy;
 			this.radius = 4;
-			
-			this.xsp = 2;
-			this.ysp = 2;
+
+			this.xsp = 4;
+			this.ysp = -4;
 		}
-		
-		private void update(){
-			if (collisionCircleLine(ball,paddle.x,paddle.y,paddle.x+paddle.width,paddle.y)){
+		private void transformDirection(double N){
+			double c_speed = dist(0,0,xsp,ysp);
+			double c_angle = Math.atan2(-this.ysp, -this.xsp)*180/Math.PI;
+			double e_angle = (2*N-c_angle-180)*Math.PI/180;
+			
+			xsp = c_speed*Math.cos(e_angle);
+			ysp = c_speed*Math.sin(e_angle);
+			
+		}
+		private Line checkPlace(int cx, int cy) {
+			for (Block b : blockList) {
+				if (collisionRectRect(this.x - this.radius + cx, this.y
+						- this.radius + cy, this.x + this.radius + cx, this.y
+						+ this.radius + cy, b.x, b.y, b.x + b.width, b.y
+						+ b.height)) {
+					for (Line l : b.lines) {
+						if (collisionCircleLine(this.x + cx, this.y + cy,
+								this.radius, l.AX, l.AY, l.BX, l.BY)) {
+							return l;
+						}
+					}
+				}
+			}
+			return null;
+		}
+		private void bounce(Line l){
+			transformDirection(90+Math.atan2(l.BX-l.AX, l.BY-l.AY)*180/Math.PI);
+			l.rect.hp--;
+		}
+
+		private void update() {
+			if (collisionCircleLine(ball.x, ball.y, ball.radius, paddle.x,
+					paddle.y, paddle.x + paddle.width, paddle.y)) {
 				this.ysp *= -1;
 			}
-			
-			if (this.x+this.radius > screen_width && this.xsp > 0) {
+
+			if (this.x + this.radius > screen_width && this.xsp > 0) {
 				this.xsp *= -1;
-			}else if (this.x-this.radius < 0 && this.xsp < 0) {
+			} else if (this.x - this.radius < 0 && this.xsp < 0) {
 				this.xsp *= -1;
-			}else if (this.y-this.radius < 0 && this.ysp < 0) {
+			} else if (this.y - this.radius < 0 && this.ysp < 0) {
 				this.ysp *= -1;
-			}else if (this.y+this.radius > screen_height && this.ysp > 0) {
-				//game over
+			} else if (this.y + this.radius > screen_height && this.ysp > 0) {
+				// game over
 			}
 			
-			this.x += this.xsp;
-			this.y += this.ysp;
+			//transformDirection(0);
+
+			Line tempL = null;
+			if (xsp > 0) {
+				for (int i = 0; i < xsp; i++) {
+					tempL = checkPlace(1, 0);
+					if (tempL != null) {
+						bounce(tempL);
+						break;
+					}
+					x++;
+				}
+			} else if (xsp < 0) {
+				for (int i = 0; i < -xsp; i++) {
+					tempL = checkPlace(-1, 0);
+					if (tempL != null) {
+						bounce(tempL);
+						break;
+					}
+					x--;
+				}
+			}
+			if (ysp > 0) {
+				for (int i = 0; i < ysp; i++) {
+					tempL = checkPlace(0, 1);
+					if (tempL != null) {
+						bounce(tempL);
+						break;
+					}
+					y++;
+				}
+			} else if (ysp < 0) {
+				for (int i = 0; i < -ysp; i++) {
+					tempL = checkPlace(0, -1);
+					if (tempL != null) {
+						bounce(tempL);
+						break;
+					}
+					y--;
+				}
+			}
+
 		}
 	}
-	
-	public Game(){
-		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+
+	public Game() {
+		BufferedImage cursorImg = new BufferedImage(16, 16,
+				BufferedImage.TYPE_INT_ARGB);
 		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
-		    cursorImg, new Point(0, 0), "blank cursor");
+				cursorImg, new Point(0, 0), "blank cursor");
 		this.setCursor(blankCursor);
-		
+
 		setFocusable(true);
-        setDoubleBuffered(true);
-        
-        this.addMouseMotionListener(new ML());
-        this.addKeyListener(new KL());
-		
-        timer = new Timer(0, loop);
-        timer.setInitialDelay(1000);
-        timer.start();
-        
-        Timer t = new Timer(1000, updateFPS);
-    	t.setInitialDelay(0);
-        t.start();
-        
-        ball = new Ball(128,128);
-        paddle = new Paddle(256,540,64,8);
-        lastUpdateTime = System.nanoTime();
-        
-	}
-	
-	ActionListener loop = new ActionListener()
-    {   
-        @Override
-        public void actionPerformed(ActionEvent event)
-        {
-        	
-        	//loop
-        	paddle.moveTo(pos_x-paddle.width/2);
-        	ball.update();
-        	repaint();
-        	totalFrameCount++;
-        	
-        	long l = (long) (((1000000000/fps) - (System.nanoTime() - lastUpdateTime))/1000000);
-        	System.out.println(l);
-        	try {
-        		if (l > 0)
-        			Thread.sleep(l);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+		this.addMouseMotionListener(new ML());
+		this.addKeyListener(new KL());
+		blockList = new ArrayList<Block>();
+
+		Timer t = new Timer(1000, updateFPS);
+		t.setInitialDelay(0);
+		t.start();
+
+		ball = new Ball(128, 500);
+		paddle = new Paddle(256, 540, 64, 8);
+
+		for (int j = 1; j < 7; j++) {
+			for (int i = 0; i < 20; i++) {
+				Block block = new Block(32 + i * 33, 32 + j * 17, 32, 16, j);
+				blockList.add(block);
 			}
-        	lastUpdateTime = System.nanoTime();
-        	
-        	
-        	
-        }
-    };
-	
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.drawRect(pos_x, pos_y, 64, 64);
-        screen_width = this.getWidth();
-    	screen_height = this.getHeight();
-        
-        g2.setColor(Color.BLACK);
-        g2.fillRect(0, 0, screen_width, screen_height);
-        
-        g2.setColor(Color.WHITE);
-        g2.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
-        g2.fillOval(ball.x-ball.radius, ball.y-ball.radius, 2*ball.radius, 2*ball.radius);
-        
-        g2.setColor(Color.BLUE);
-        /*for (Rect r : rectList){
-        	//g2.fillRect(r.x, r.y, r.width, r.height);
-    	}*/
+		}
+
 	}
-	
-	public static void main(String args[]){
-		JFrame window = new JFrame("Test Containment");
+
+	public void run() {
+
+		long last_loop = System.nanoTime();
+		final int target_fps = 60;
+		final long optimal_time = 1000000000 / target_fps;
+
+		while (running) {
+
+			long now = System.nanoTime();
+			last_loop = now;
+
+			update();
+			repaint();
+
+			try {
+				Thread.sleep((last_loop - System.nanoTime() + optimal_time) / 1000000);
+			} catch (InterruptedException e) {
+				System.out.println("interrupted");
+			}
+			lastUpdateTime = System.currentTimeMillis();
+		}
+	}
+
+	public void update() {
+		paddle.moveTo(pos_x - paddle.width / 2);
+		ball.update();
+		totalFrameCount++;
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		Font font = new Font("Serif", Font.PLAIN, 14);
+		g2.setFont(font);
+
+		g2.drawRect(pos_x, pos_y, 64, 64);
+		screen_width = this.getWidth();
+		screen_height = this.getHeight();
+
+		g2.setColor(Color.BLACK);
+		g2.fillRect(0, 0, screen_width, screen_height);
+
+		if (blockList.size() > 0) {
+			for (Block b : blockList) {
+				switch (b.hp) {
+				case 1:
+					g2.setColor(Color.RED);
+					break;
+				case 2:
+					g2.setColor(Color.ORANGE);
+					break;
+				case 3:
+					g2.setColor(Color.YELLOW);
+					break;
+				case 4:
+					g2.setColor(Color.GREEN);
+					break;
+				case 5:
+					g2.setColor(Color.BLUE);
+					break;
+				case 6:
+					g2.setColor(Color.MAGENTA);
+					break;
+				case 7:
+					g2.setColor(Color.lightGray);
+					break;
+				default:
+					g2.setColor(Color.WHITE);
+					break;
+				}
+				g2.fillRect(b.x, b.y, b.width, b.height);
+				g2.setColor(Color.PINK);
+				for (Line l : b.lines) {
+					g2.drawLine(l.AX, l.AY, l.BX, l.BY);
+				}
+			}
+		}
+
+		g2.setColor(Color.WHITE);
+		g2.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+		g2.fillOval(ball.x - ball.radius, ball.y - ball.radius,
+				2 * ball.radius, 2 * ball.radius);
+		g2.drawString("FPS: " + String.valueOf(current_fps), 8, 18);
+
+	}
+
+	public static void main(String args[]) {
+		JFrame window = new JFrame("Breakout");
+		window.setLocationByPlatform(true);
 		window.setSize(800, 600);
 		window.setContentPane(new Game());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 	}
-	
-	public class ML extends MouseMotionAdapter
-	{
+
+	public class ML extends MouseMotionAdapter {
 		public void mouseMoved(MouseEvent e) {
 			pos_x = e.getX();
 			pos_y = e.getY();
 		}
 	}
 
-	public class KL extends KeyAdapter
-	{
+	public class KL extends KeyAdapter {
 		public void keyPressed(KeyEvent e) {
 			int key = e.getKeyCode();
-			
+
 			if (key == KeyEvent.VK_LEFT) {
-				paddle.moveTo(paddle.x-paddle.hsp);
-			}else if (key == KeyEvent.VK_RIGHT) {
-				paddle.moveTo(paddle.x+paddle.hsp);
+				paddle.moveTo(paddle.x - paddle.hsp);
+			} else if (key == KeyEvent.VK_RIGHT) {
+				paddle.moveTo(paddle.x + paddle.hsp);
 			}
-			
+
 		}
 	}
+
 }
-				
